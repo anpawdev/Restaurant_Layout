1b0a70c468e49bc09ed150c7b5efff86797b0fea
Anna Pawlik <pawlikanna24@gmail.com>
2020-11-02 12:04:33 +0100

    Add form validation

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         showImageNumberLabel: true,
     });
 });
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    const forms = document.getElementsByClassName('needs-validation');
+    const validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
